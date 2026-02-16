f5cbeadc8e7374549f0be8c9c24861b96bf22742
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2026-02-16 15:10:02 +0300

    Implement Level Map UI and integrate with level system
    - Implemented Level Map user interface
    - Added level selection functionality
    - Connected Level Map with Episode and LevelSequenceController
    - Enabled loading selected levels from the map
    - Integrated map flow with existing episode progression system
    - Ensured correct interaction between UI and scene transitions

---
+++ Assets/MapLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using SpaceShooter;
+
+//Этот скрипт загружает следующий уровень, по которому я кликнул
+
+namespace TowerDefense
+{
+    public class MapLevel : MonoBehaviour
+    {
+        [SerializeField] private Episode episode;
+        public void LoadLevel()
+        {
+            LevelSequenceController.Instance.StartEpisode(episode);
+        }
+    }
+}
--- Assets/TimeLevelCondition.cs
+++ Assets/TimeLevelCondition.cs
@@ -6,5 +6,9 @@ using SpaceShooter;
 public class TimeLevelCondition : MonoBehaviour, ILevelCondition
 {
     [SerializeField] private float timeLimit = 4f;
+    private void Start()
+    {
+        timeLimit += Time.time;
+    }
     public bool IsCompleted => Time.time > timeLimit;
 }
