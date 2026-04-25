e1fc7a44031f5b1ec4dfecc840a5ed6c246bdfe1
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2026-04-25 11:18:19 +0300

    Implement global sound system with enum-based playback and Unity inspector hooks
    Added Sound enum containing all game sound types.
    
    Implemented SoundPlayer singleton to manage AudioSource playback and map each Sound to its corresponding AudioClip.
    
    Designed an inspector-friendly configuration interface for assigning clips inside Unity.
    
    Added C# extension methods so sounds can be played with a simple global call: Sound.<Name>.Play();
    
    Created SoundHook component for triggering sounds directly from Unity inspector events such as Button.OnClick or Destructible.OnDeath.
    
    Ensured multiple sounds can play simultaneously and continue playing even if the caller object is destroyed.
    
    Configured sounds as non-spatial (2D audio only, no 3D positioning).
    
    Tested the system using dedicated test buttons.

---
+++ Assets/Sound.cs
@@ -0,0 +1,21 @@
+﻿//здесь используется базовый функционал C# и больше ничего
+namespace TowerDefense
+{
+    public enum Sound    // enum и его методы (см. ниже)
+    {
+        BGM,
+        Arrow,
+        Arrowhit,
+    }
+    public static class SoundExtensions  //нужен статичный класс, внутри которого есть статичный метод
+        //Это есть "Метод расширения".  Здесь мы имеем метод расширения для enum Sound.
+    {
+        public static void Play(this Sound sound) //если здесь я перед первым параметром ставлю слово this,
+            //то после этого метод можно вызывать с этого параметра. 
+        {
+            // Sound.BGM.Play();  //То есть беру одного из enum-ов и на нём что-то вызываю.
+            SoundPlayer.Instance.Play(sound);
+        }
+    }
+}
+
---
+++ Assets/Sounds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    [CreateAssetMenu()]
+    public class Sounds : ScriptableObject
+    {
+        public AudioClip[] m_Sounds;
+        public AudioClip this[Sound s] => m_Sounds[(int)s]; //Наш объект со звуками теперь индексируется
+    }
+}
+
